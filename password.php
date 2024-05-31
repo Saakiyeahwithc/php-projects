b92b011a79a73dd173cbb63ae7d9619aa058d7b6
saakiyeahwith.c <prashant1.785401@cct.tu.edu.np>
2024-05-31 18:28:36 +0545

    second commit

--- password.php
+++ password.php
@@ -1,1 +1,27 @@
 <?php
+session_start();
+
+$correctpass = "PotatoFanny123";
+$max_attempts = 5;
+
+if (!isset($_SESSION['attempts'])) {
+    $_SESSION['attempts'] = 0;
+}
+if (isset($_SERVER['REQUEST_METHOD'])) {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $_SESSION['attempts']++;
+
+        $user_pass = isset($_POST['password']) ? $_POST['password'] : "default";
+
+        if ($user_pass === $correctpass) {
+            echo "Access Granted";
+            $_SESSION['attempts'] = 0;
+        } else {
+            if ($_SESSION['attempts'] >= $max_attempts) {
+                echo " Maximum attempts exceeded. Access denied";
+            } else {
+                echo "Incorrect password. Attempt " . $_SESSION['attempts'] . " of $max_attempts.";
+            }
+        }
+    }
+}
