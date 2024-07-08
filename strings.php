55384d38b0624dffc10ade2396bbecf614930624
saakiyeahwith.c <prashant1.785401@cct.tu.edu.np>
2024-07-08 22:59:04 +0545

    addedExamples

--- strings.php
+++ strings.php
@@ -8,16 +8,17 @@
 
 //regular expressions(reg_Ex)
 
-
+//preg_match_all()
 // $str = "here we are reading";
 // $patern = "/re/i";
 // echo preg_match_all($pattern, $str);
 
-
+//preg_replace()
 // $str = "visit course page via url for daily update";
 // $pattern = "/url/i";
 // echo preg_replace($pattern, "neupaneprakash.github.io/web.html", $str);
 
+//preg_match()
 // $name_pattern = '/[A-Za-z ]+$/';
 // $name = 'Prashant Shakya';
 
@@ -26,3 +27,23 @@
 // } else {
 //     echo 'Invalid name';
 // }
+
+
+//cleaninagStrings
+//1.Removing Whitespace
+
+// $title = "Good Afternoon Class!";
+// $str1 = ltrim($title);
+// $str2 = rtrim($title);
+// $str3 = trim($title);
+// echo "\n $str1,\n $str2,\n $str3\n";
+
+
+//2.Changing Case
+
+// $string1 = "CCT web\n";
+// $string2 = "Bit fifth\n";
+// print(strtolower($string1));
+// print(strtoupper($string1));
+// print(ucfirst($string2));
+// print(ucwords($string2));
