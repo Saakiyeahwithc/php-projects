29599267ad8531e715cbdfdaf48b9cb361222adc
saakiyeahwith.c <prashant1.785401@cct.tu.edu.np>
2024-07-08 13:12:46 +0545

    php regex

---
+++ strings.php
@@ -0,0 +1,28 @@
+
+<?php
+// $string = 'Hello';
+// for($i=0;$i<strlen($string);$i++)
+//     printf("The %dth character is %s\n",$i, $string[$i]); 
+
+
+
+//regular expressions(reg_Ex)
+
+
+// $str = "here we are reading";
+// $patern = "/re/i";
+// echo preg_match_all($pattern, $str);
+
+
+// $str = "visit course page via url for daily update";
+// $pattern = "/url/i";
+// echo preg_replace($pattern, "neupaneprakash.github.io/web.html", $str);
+
+// $name_pattern = '/[A-Za-z ]+$/';
+// $name = 'Prashant Shakya';
+
+// if (preg_match($name_pattern, $name)) {
+//     echo 'Valid name';
+// } else {
+//     echo 'Invalid name';
+// }
