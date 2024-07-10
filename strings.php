d3db35613a8fac4c167a2845864216b52ab71f7f
saakiyeahwith.c <prashant1.785401@cct.tu.edu.np>
2024-07-10 14:02:33 +0545

    Strings

--- strings.php
+++ strings.php
@@ -47,3 +47,27 @@
 // print(strtoupper($string1));
 // print(ucfirst($string2));
 // print(ucwords($string2));
+
+//validating email
+
+// $email_pattern = '/^[A-Za-z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/';
+// $email = 'prashantshakya404@gmail.com';
+// if (preg_match($email_pattern, $email)) {
+//     echo 'valid Email';
+// } else {
+//    echo 'Invalid Email';
+// }
+
+
+// $matrix = array(
+//     array(1, 2, 3),
+//     array(4, 5, 6),
+//     array(7, 8, 9)
+// );
+// $extractedValues = array();
+// foreach ($matrix as $row) {
+//     foreach ($row as $value) {
+//         $extractedValues[] = $value;
+//     }
+// }
+// echo $extractedValues[5];
